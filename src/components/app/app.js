8f71d4205422a66b590636e78a9b15f1fdcf26ea
korsakov <korsakov.ua.dn@gmail.com>
2021-03-28 20:41:16 +0300

    end HW

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,100 @@
-import React from 'react';
-import {Col, Row, Container} from 'reactstrap';
+import React, { Component } from 'react';
+import {Col, Row, Container, Button} from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
+import './app.css';
+import gotService from '../../services/gotService';
+import Spinner from '../spinner';
+import ErrorMessage from '../errorMessage';
 
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+
+export default class App extends Component{
+    constructor() {
+        super();
+        this.updateChar();
+    }
+
+    gotService = new gotService();
+
+    state = {
+        className: "show",
+        char: {},
+        loading: true,
+        error: false
+    }
+
+    onCharLoaded = (char) => {
+        this.setState({
+            char,
+            loading: false
+        });
+    }
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false
+        })
+    }
+    updateChar = () => {
+        const id = Math.floor(Math.random()*140 + 25); // начиная с 25 до 140
+        this.gotService.getCharacter(id)
+            .then(this.onCharLoaded)
+            .catch(this.onError);
+    }
+
+    onToggleChar = () => {
+        const {className} = this.state;
+        if (className === "show") {
+            this.setState({
+                className: "hide",
+                loading: true
+            })
+        } else {
+            this.setState({
+                className: "show"
+            })
+            this.updateChar();
+        }
+    }
+
+    render() {
+        const {className, char, loading, error} = this.state
+
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <RandomChar char={char}/> : null;
+
+        return (
+            <> 
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                            <div className={className}>
+                                {errorMessage}
+                                {spinner}
+                                {content}
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Button className="btn" color="primary" onClick={this.onToggleChar}>Toogle randome character</Button>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
 };
-
-export default App;
--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,65 +1,93 @@
-import React, {Component} from 'react';
-import gotService from '../../services/gotService';
+import React from 'react';
+// import gotService from '../../services/gotService';
 import './randomChar.css';
-import Spinner from '../spinner';
-import ErrorMessage from '../errorMessage';
 
-export default class RandomChar extends Component {
-    constructor() {
-        super()
-        this.updateChar();
-    }
 
-    gotService = new gotService();
-    state = {
-        char: {},
-        loading: true,
-        error: false
-    }
-    onCharLoaded = (char) => {
-        this.setState({
-            char,
-            loading: false
-        });
-    }
-    onError = (err) => {
-        this.setState({
-            error: true,
-            loading: false
-        })
-    }
-    updateChar() {
-        const id = Math.floor(Math.random()*140 + 25); // начиная с 25 до 140
-        this.gotService.getCharacter(id)
-            .then(this.onCharLoaded)
-            .catch(this.onError);
-    }
+// export default class RandomChar extends Component {
+//     constructor() {
+//         super()
+//         this.updateChar();
+//     }
 
-    render() {
-        const { char, loading, error } = this.state
-        // if (loading) {
-        //     return <Spinner/> // если функция render увидит return далее код не пойдет
-        // }
-        const errorMessage = error ? <ErrorMessage/> : null;
-        const spinner = loading ? <Spinner/> : null;
-        const content = !(loading || error) ? <View char={char}/> : null;
-        // тернарный оператор если - то - иначе (= ? :)
+//     gotService = new gotService();
+//     state = {
+//         char: {},
+//         loading: true,
+//         error: false
+//     }
+//     onCharLoaded = (char) => {
+//         this.setState({
+//             char,
+//             loading: false
+//         });
+//     }
+//     onError = (err) => {
+//         this.setState({
+//             error: true,
+//             loading: false
+//         })
+//     }
+//     updateChar() {
+//         const id = Math.floor(Math.random()*140 + 25); // начиная с 25 до 140
+//         this.gotService.getCharacter(id)
+//             .then(this.onCharLoaded)
+//             .catch(this.onError);
+//     }
+
+//     render() {
+//         const { char, loading, error,} = this.state
+//         // if (loading) {
+//         //     return <Spinner/> // если функция render увидит return далее код не пойдет
+//         // }
+//         const errorMessage = error ? <ErrorMessage/> : null;
+//         const spinner = loading ? <Spinner/> : null;
+//         const content = !(loading || error) ? <View char={char}/> : null;
+//         // тернарный оператор если - то - иначе (= ? :)
         
-        return (
-            <div className="random-block rounded">
-                {errorMessage}
-                {spinner}
-                {content}
-            </div>
-        );
-    }
-}
+//         return (
+//             <div className="random-block rounded">
+//                 {errorMessage}
+//                 {spinner}
+//                 {content}
+//             </div>
+//         );
+//     }
+// }
 
-const View = ({char}) => {
+// const View = ({char}) => {
+//     const {name, gender, born, died, culture} = char;
+//     // console.log(char);
+//     return (
+//         <>
+//             <h4>Random Character: {name}</h4>
+//             <ul className="list-group list-group-flush">
+//                 <li className="list-group-item d-flex justify-content-between">
+//                     <span className="term">Gender </span>
+//                     <span>{gender}</span>
+//                 </li>
+//                 <li className="list-group-item d-flex justify-content-between">
+//                     <span className="term">Born </span>
+//                     <span>{born}</span>
+//                 </li>
+//                 <li className="list-group-item d-flex justify-content-between">
+//                     <span className="term">Died </span>
+//                     <span>{died}</span>
+//                 </li>
+//                 <li className="list-group-item d-flex justify-content-between">
+//                     <span className="term">Culture </span>
+//                     <span>{culture}</span>
+//                 </li>
+//             </ul>
+//         </>
+//     )
+// } 
+
+const RandomChar = ({char}) => {
+    
     const {name, gender, born, died, culture} = char;
-    // console.log(char);
+       
     return (
-        <>
+        <div className="random-block rounded">
             <h4>Random Character: {name}</h4>
             <ul className="list-group list-group-flush">
                 <li className="list-group-item d-flex justify-content-between">
@@ -79,6 +107,8 @@ const View = ({char}) => {
                     <span>{culture}</span>
                 </li>
             </ul>
-        </>
+        </div>
     )
-} 
+}
+
+export default RandomChar;
