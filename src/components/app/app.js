1dbe4e6b6f40d40c4808cd85aa558431560848f6
Matrix <o.korsakov@matrixb2b.net>
2021-04-05 16:50:04 +0300

    debug bg img

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -15,7 +15,7 @@ export default class App extends Component {
     gotService = new gotService();
 
     state = {
-        showRandomChar: true,
+        showRandomChar: false,
         error: false,
     }
 
